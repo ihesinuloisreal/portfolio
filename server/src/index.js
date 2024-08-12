6602e07dcffe618884b1053830ec5003ce96ac5f
IhesinuloIsreal <IhesinuloIsreali@gmail.com>
2024-08-12 00:08:08 +0000

    update

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = require('./app')
 const PORT = 5000
@@ -10,7 +11,7 @@ main().catch(err => console.log(err));
 const server = http.createServer(app)
 
 async function main() {
-    await mongoose.connect('mongodb+srv://ihesinuloisreal:yahweh0852@profile.fnozd.mongodb.net/');
+    await mongoose.connect(process.env.MONGO_DB);
     console.log("Connected to Database");
   
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
